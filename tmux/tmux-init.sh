3636b50354438cb86c57be7ecafc1b6c79931ea8
Daniel Liu <idaniel.liu@gmail.com>
2026-03-29 19:52:55 -0700

    Claude/AI setups, git alias updates, gitignore for claude/ai

--- tmux/tmux-init.sh
+++ tmux/tmux-init.sh
@@ -22,10 +22,10 @@ workm="fractional-m"
 # backend
 tmux new-session -d -s $workb -n "gql"
 tmux new-window -t $workb:1 -n server
-tmux new-window -t $workb:2 -n worker
-tmux new-window -t $workb:3 -n code
-tmux new-window -t $workb:4 -n code2
-tmux new-window -t $workb:5 -n code3
+tmux new-window -t $workb:2 -n claude
+tmux new-window -t $workb:3 -n claude2
+tmux new-window -t $workb:4 -n code
+tmux new-window -t $workb:5 -n code2
 tmux new-window -t $workb:6 -n notes
 tmux new-window -t $workb:7 -n db
 
@@ -40,8 +40,8 @@ tmux send-keys -t $workb:7 "source ~/.zshrc.work" C-m
 
 tmux send-keys -t $workb:0 "cdfb" C-m
 tmux send-keys -t $workb:1 "cdfbs" C-m
-tmux send-keys -t $workb:2 "cdfbs" C-m
-tmux send-keys -t $workb:3 "cdfbs" C-m
+tmux send-keys -t $workb:2 "cdfb" C-m
+tmux send-keys -t $workb:3 "cdfb" C-m
 tmux send-keys -t $workb:4 "cdfbs" C-m
 tmux send-keys -t $workb:5 "cdfbs" C-m
 tmux send-keys -t $workb:6 "cdfnotes" C-m
@@ -49,9 +49,9 @@ tmux send-keys -t $workb:7 "cdfb" C-m
 
 # frontend
 tmux new-session -d -s $workf -n serve
-tmux new-window -t $workf:1 -n code
-tmux new-window -t $workf:2 -n code2
-tmux new-window -t $workf:3 -n code3
+tmux new-window -t $workf:1 -n claude
+tmux new-window -t $workf:2 -n code
+tmux new-window -t $workf:3 -n code2
 
 tmux send-keys -t $workf:0 "source ~/.zshrc.work" C-m
 tmux send-keys -t $workf:1 "source ~/.zshrc.work" C-m
@@ -59,7 +59,7 @@ tmux send-keys -t $workf:2 "source ~/.zshrc.work" C-m
 tmux send-keys -t $workf:3 "source ~/.zshrc.work" C-m
 
 tmux send-keys -t $workf:0 "cdfff" C-m
-tmux send-keys -t $workf:1 "cdfff" C-m
+tmux send-keys -t $workf:1 "cdff" C-m
 tmux send-keys -t $workf:2 "cdfff" C-m
 tmux send-keys -t $workf:3 "cdfff" C-m
 
